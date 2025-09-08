8693e606aec38111c31281292b240752d46e16fb
DustVeer <dveerdonk@gmail.com>
2025-09-08 11:41:32 +0200

    Added product/id search

--- src/main/java/com/plm/poelman/java_api/controllers/ProductController.java
+++ src/main/java/com/plm/poelman/java_api/controllers/ProductController.java
@@ -2,9 +2,12 @@ package com.plm.poelman.java_api.controllers;
 
 import com.plm.poelman.java_api.models.Product;
 import com.plm.poelman.java_api.repositories.ProductRepository;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -26,5 +29,12 @@ public class ProductController {
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        Optional<Product> p = productRepository.findById(id);
+        return p.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
