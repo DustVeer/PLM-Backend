dfb70d9b64f6ca9dd311f42859dfa57325d65542
DustVeer <dveerdonk@gmail.com>
2025-12-03 17:23:23 +0100

    Added EnvLoader to the Tests

--- src/test/java/com/plm/poelman/java_api/PlmPoelmanJavaApiApplicationTests.java
+++ src/test/java/com/plm/poelman/java_api/PlmPoelmanJavaApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.plm.poelman.java_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.plm.poelman.java_api.configs.EnvLoader;
+
 @SpringBootTest
 class PlmPoelmanJavaApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public static void contextLoads() {
+		EnvLoader.load();
 	}
 
 }
